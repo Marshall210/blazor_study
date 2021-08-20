83405ac320222d6d32442567a52e81c00ae35e4e
51099ihtfp51099G <tymchenko99978@gmail.com>
2021-08-20 16:19:37 +0300

    1_2les_v1

---
+++ Blazor1/Models/Page.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Blazor1.Models
+{
+    public class Page
+    {
+        public string Name { get; set; }
+        public string Level { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
